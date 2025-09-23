ea20ae580d61451c3d2399f9cbe651a5aa6fbe57
AkshayMohan <mohanakshaykannur@gmail.com>
2025-09-23 20:34:17 +0530

    sorting 1

---
+++ Sorting/Selection.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class Selection {
+    public static void main(String[] args) {
+        int[] arr = {7,8,3,1,2};
+
+        for (int i = 0; i < arr.length-1; i++) {
+            int smallest = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[smallest]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
